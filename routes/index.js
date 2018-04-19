d5a6576561384b3ece2e6b2b669a87e2f52a2d77
manfwh <841533182@qq.com>
2018-04-19 10:06:35 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -121,7 +121,7 @@ router.post('/formid', async (ctx, next) => {
 
   setTimeout(() => {
     sendTemp(openid, ctx.access_token, template_id, formid, template)
-  }, 5000); //day * 24 * 60 * 60 * 1000
+  }, day * 24 * 60 * 60 * 1000); //
 
 
   function sendTemp(openid, token, template_id, form_id, data) {
